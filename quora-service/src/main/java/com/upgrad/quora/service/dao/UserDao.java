b86a603a2568b257b88317fbdd98e31188941820
UGjaguar <rakesh.pujari1109@gmail.com>
2021-01-24 23:31:18 +0800

    added exception handling

--- quora-service/src/main/java/com/upgrad/quora/service/dao/UserDao.java
+++ quora-service/src/main/java/com/upgrad/quora/service/dao/UserDao.java
@@ -2,9 +2,10 @@ package com.upgrad.quora.service.dao;
 
 import com.upgrad.quora.service.entity.UserEntity;
 import org.springframework.stereotype.Repository;
-
+import com.upgrad.quora.service.entity.UserAuthEntity;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.NoResultException;
 
 
 @Repository
@@ -13,12 +14,52 @@ public class UserDao {
   @PersistenceContext
   private EntityManager entityManager;
 
+  public UserEntity getUserById(final String uuid) {
+    try {
+      return entityManager.createNamedQuery("userByUuid", UserEntity.class).setParameter("uuid", uuid).getSingleResult();
+    } catch (NoResultException nre) {
+      return null;
+    }
+  }
+
   public UserEntity createUser(UserEntity userEntity) {
-    entityManager.persist(userEntity);
-    return userEntity;
+    try {
+      entityManager.persist(userEntity);
+      return userEntity;
+    } catch (Exception e) {
+      return null;
+    }
   }
 
   public UserEntity getUserByUsername(final String username) {
-    return entityManager.createNamedQuery("userByUsername", UserEntity.class).setParameter("username", username).getSingleResult();
+    try {
+      return entityManager.createNamedQuery("userByUsername", UserEntity.class).setParameter("username", username).getSingleResult();
+    } catch (NoResultException nre) {
+      return null;
+    }
+  }
+
+  public UserAuthEntity createAuth(final UserAuthEntity userAuthEntity) {
+    entityManager.persist(userAuthEntity);
+    return userAuthEntity;
+  }
+
+  public Boolean isUsernameExists(final String username) {
+    try {
+      UserEntity singleResult = entityManager.createNamedQuery("userByUsername", UserEntity.class).setParameter("username", username).getSingleResult();
+      return true;
+    } catch (NoResultException nre) {
+      return false;
+    }
+  }
+
+  public Boolean isEmailExists(final String email) {
+    try {
+      UserEntity singleResult = entityManager.createNamedQuery("userByEmail", UserEntity.class).setParameter("email", email).getSingleResult();
+      return true;
+    } catch (NoResultException nre) {
+      return false;
+    }
+
   }
 }
