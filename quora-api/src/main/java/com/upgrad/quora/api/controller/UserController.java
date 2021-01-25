8283628c399175c7a3386e261217513be95935a0
Mishal Menezes <mishmenez@gmail.com>
2021-01-25 12:07:15 +0530

    Update signup request

--- quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
@@ -1,12 +1,16 @@
 package com.upgrad.quora.api.controller;
 
 import com.upgrad.quora.api.model.SigninResponse;
+import com.upgrad.quora.api.model.SignoutResponse;
 import com.upgrad.quora.api.model.SignupUserRequest;
 import com.upgrad.quora.api.model.SignupUserResponse;
 import com.upgrad.quora.service.entity.UserAuthEntity;
 import com.upgrad.quora.service.entity.UserEntity;
 import com.upgrad.quora.service.business.UserBusinessService;
 import com.upgrad.quora.service.exception.AuthenticationFailedException;
+import com.upgrad.quora.service.exception.SignOutRestrictedException;
+import com.upgrad.quora.service.exception.SignUpRestrictedException;
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -47,23 +51,27 @@ public class UserController {
   }
 
 
+  /*
+  * Sign up method called for the endpoint "/user/signup"
+  * */
+
   @RequestMapping(method = RequestMethod.POST, path = "signup", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-  public ResponseEntity<SignupUserResponse> signup(@RequestBody SignupUserRequest signupUserRequest) {
+  public ResponseEntity<SignupUserResponse> signup(final SignupUserRequest signupUserRequest) throws SignUpRestrictedException {
 
 
-    final UserEntity userEntity = new UserEntity();
+      final UserEntity userEntity = new UserEntity();
 
-    userEntity.setUuid(UUID.randomUUID().toString());
-    userEntity.setFirstname(signupUserRequest.getFirstName());
-    userEntity.setLastname(signupUserRequest.getLastName());
-    userEntity.setUsername(signupUserRequest.getUserName());
-    userEntity.setEmail(signupUserRequest.getEmailAddress());
-    userEntity.setPassword(signupUserRequest.getPassword());
-    userEntity.setCountry(signupUserRequest.getCountry());
-    userEntity.setAboutme(signupUserRequest.getAboutMe());
-    userEntity.setDob(signupUserRequest.getDob());
-    userEntity.setRole("nonadmin");
-    userEntity.setContactnumber(signupUserRequest.getContactNumber());
+      userEntity.setUuid(UUID.randomUUID().toString());
+      userEntity.setFirstname(signupUserRequest.getFirstName());
+      userEntity.setLastname(signupUserRequest.getLastName());
+      userEntity.setUsername(signupUserRequest.getUserName());
+      userEntity.setEmail(signupUserRequest.getEmailAddress());
+      userEntity.setPassword(signupUserRequest.getPassword());
+      userEntity.setCountry(signupUserRequest.getCountry());
+      userEntity.setAboutme(signupUserRequest.getAboutMe());
+      userEntity.setDob(signupUserRequest.getDob());
+      userEntity.setRole("nonadmin");
+      userEntity.setContactnumber(signupUserRequest.getContactNumber());
 
     final UserEntity createdUserEntity = userBusinessService.signup(userEntity);
 
@@ -71,5 +79,20 @@ public class UserController {
 
     return new ResponseEntity<SignupUserResponse>(userResponse, HttpStatus.CREATED);
 
+  }
+
+
+
+  /*
+   * Sign out method called for the endpoint "/user/signout"
+   * */
+
+  @RequestMapping(method = RequestMethod.POST, path = "signout")
+  public ResponseEntity<SignoutResponse> signout(@RequestHeader("authorization") final String accessToken) throws SignOutRestrictedException {
+
+    String signedOutUser = userBusinessService.signout(accessToken);
+    SignoutResponse signoutResponse = new SignoutResponse().id(signedOutUser).message("SIGNED OUT SUCCESSFULLY");
+
+    return new ResponseEntity<SignoutResponse>(signoutResponse, HttpStatus.OK);
   }
 }
