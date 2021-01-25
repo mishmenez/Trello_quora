b89f4ed8eb001c9c223472e8e8130ba54dff3132
Mishal Menezes <mishmenez@gmail.com>
2021-01-25 16:07:11 +0530

    common & answer controller added

--- quora-api/src/main/java/com/upgrad/quora/api/controller/CommonController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/CommonController.java
@@ -3,6 +3,7 @@ package com.upgrad.quora.api.controller;
 import com.upgrad.quora.api.model.UserDetailsResponse;
 import com.upgrad.quora.service.business.UserBusinessService;
 import com.upgrad.quora.service.entity.UserEntity;
+import com.upgrad.quora.service.exception.AuthorizationFailedException;
 import com.upgrad.quora.service.exception.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,30 +14,29 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/")
 public class CommonController {
 
-    @Autowired
-    private UserBusinessService userBusinessService;
+  @Autowired
+  private UserBusinessService userBusinessService;
 
-    @RequestMapping(method = RequestMethod.GET, path = "/userprofile/{userId}")
-    public ResponseEntity<UserDetailsResponse> getUserById(@PathVariable("userId") final String userUuid, @RequestHeader("authorization") final String authorization) throws UserNotFoundException {
+  @RequestMapping(method = RequestMethod.GET, path = "/userprofile/{userId}")
+  public ResponseEntity<UserDetailsResponse> getUserById(@PathVariable("userId") final String userUuid, @RequestHeader("authorization") final String authorization)
+          throws UserNotFoundException, AuthorizationFailedException {
 
-        System.out.println(userUuid);
-        System.out.println("authorization >>> " + authorization.toString());
 
-        UserEntity userById = userBusinessService.getUserById(userUuid);
+    UserEntity userById = userBusinessService.getUserProfile(userUuid, authorization);
 
-        UserDetailsResponse userDetailsResponse = new UserDetailsResponse();
+    UserDetailsResponse userDetailsResponse = new UserDetailsResponse();
 
-        userDetailsResponse.setFirstName(userById.getFirstname());
-        userDetailsResponse.setLastName(userById.getLastname());
-        userDetailsResponse.setUserName(userById.getUsername());
-        userDetailsResponse.setEmailAddress(userById.getEmail());
-        userDetailsResponse.setCountry(userById.getCountry());
-        userDetailsResponse.setAboutMe(userById.getAboutme());
-        userDetailsResponse.setContactNumber(userById.getContactnumber());
-        userDetailsResponse.setDob(userById.getDob());
+    userDetailsResponse.setFirstName(userById.getFirstname());
+    userDetailsResponse.setLastName(userById.getLastname());
+    userDetailsResponse.setUserName(userById.getUsername());
+    userDetailsResponse.setEmailAddress(userById.getEmail());
+    userDetailsResponse.setCountry(userById.getCountry());
+    userDetailsResponse.setAboutMe(userById.getAboutme());
+    userDetailsResponse.setContactNumber(userById.getContactnumber());
+    userDetailsResponse.setDob(userById.getDob());
 
-        return new ResponseEntity<UserDetailsResponse>(userDetailsResponse, HttpStatus.OK) ;
+    return new ResponseEntity<UserDetailsResponse>(userDetailsResponse, HttpStatus.OK) ;
 
-    }
+  }
 
 }
